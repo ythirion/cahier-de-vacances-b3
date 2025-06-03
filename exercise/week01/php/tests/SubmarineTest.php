513559262d575662d0e977af9224c73e5c760b8b
Yoan Thirion <yoan.thirion@gmail.com>
2025-06-03 08:32:07 +0200

    feat: add php for week 1

---
+++ exercise/week01/php/tests/SubmarineTest.php
@@ -0,0 +1,6 @@
+<?php
+
+test('should move on given text instructions', function () {
+    // TODO: Implement the submarine logic and replace this placeholder value
+    expect(43)->toBe(42, 'it is universal answer');
+});
