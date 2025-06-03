cb1e0351414395b0f2771edec6a72a526c8f34c1
Yoan Thirion <yoan.thirion@gmail.com>
2025-06-03 08:23:59 +0200

    feat: add week 2 in c#

---
+++ exercise/week02/c#/Shift/Vertical.cs
@@ -0,0 +1,35 @@
+﻿namespace Shift;
+
+public static class Vertical
+{
+    public static int WhichFloor(string signalStream)
+    {
+        List<Tuple<char, int>> val = [];
+
+        for (int i = 0; i < signalStream.Length; i++)
+        {
+            var c = signalStream[i];
+
+            if (signalStream.Contains("🧝"))
+            {
+                int j;
+                if (c == ')') j = 3;
+                else j = -2;
+
+                val.Add(new Tuple<char, int>(c, j));
+            }
+            else if (!signalStream.Contains("🧝"))
+            {
+                val.Add(new Tuple<char, int>(c, c == '(' ? 1 : -1));
+            }
+            else val.Add(new Tuple<char, int>(c, c == '(' ? 42 : -2));
+        }
+
+        int result = 0;
+        foreach (var kp in val)
+        {
+            result += kp.Item2;
+        }
+        return result;
+    }
+}
