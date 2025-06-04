c65e020e99be4e8322ac1597d499cd7592dacd4a
Yoan Thirion <yoan.thirion@gmail.com>
2025-06-04 07:13:53 +0200

    fix: name in week 3

--- proposed-solution/week03/features/step_definitions/rockPaperScissorsLizardSpockSteps.ts
+++ proposed-solution/week03/features/step_definitions/rockPaperScissorsLizardSpockSteps.ts
@@ -1,12 +1,12 @@
 import {Given, Then, When} from "@cucumber/cucumber";
-import {Choice, Result, RockPaperScissorsLizardSpock} from "../../src/rockPaperScissors";
+import {Choice, Result, RockPaperScissorsLizardSpock} from "../../src/rockPaperScissorsLizardSpock";
 import * as assert from "assert";
 
 let result: Result;
 let player1: Choice;
 let player2: Choice;
 
-Given(/^Player (\d+) chooses (.*)$/, function (player, choice) {
+Given(/^Player (\d+) chooses (.*)$/, function (player: number, choice: Choice) {
     if (player === 1) player1 = choice;
     else player2 = choice;
 });
@@ -15,6 +15,6 @@ When(/^they play$/, function () {
     result = RockPaperScissorsLizardSpock.play(player1, player2)
 });
 
-Then(/^the result should be (.*) because (.*)$/, function (expectedWinner, expectedReason) {
+Then(/^the result should be (.*) because (.*)$/, function (expectedWinner: number, expectedReason: number) {
     assert.deepEqual(result, {winner: expectedWinner, reason: expectedReason});
 });
